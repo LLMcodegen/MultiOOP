79898d801ac0a198a78da2e900971248cf81b1ba
LLMcodegen <wangshuai123@whu.edu.cn>
2025-06-11 21:23:18 +0800

    Update exam_run.sh

--- Auto_add_cases/exam_run.sh
+++ Auto_add_cases/exam_run.sh
@@ -1,3 +1,4 @@
-python /data0/users/wangshuai/main_file/OOP_data/example/add_exam_file.py \
-    --folder_path /data0/users/wangshuai/main_file/OOP_data/example/save_file/handle_data_79_200_re \
-    --case_n 20
+cd $current_dir
+python ./MultiOOP/Auto_add_cases/add_exam_file.py \
+    --folder_path ./MultiOOP/Auto_add_cases/sample \
+    --case_n 20# Number of test cases
